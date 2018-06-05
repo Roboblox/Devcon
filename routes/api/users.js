98cc7856ad97e5bbceadabfe3331a318f9582b01
neojohnny <schulerjohnthomas@gmail.com>
2018-06-04 17:34:44 -0700

    ok

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,37 +2,24 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const passport = require("passport");
+//const jwt = require("jsonwebtoken");
+//const keys = require("../../config/keys");
+//const passport = require("passport");
 
 // Load Input Validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+//const validateRegisterInput = require("../../validation/register");
+//const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
-
-// @route   GET api/users/test
-// @desc    Tests users route
-// @access  Public
-router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 // @route   GET api/users/register
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
-
-  // Check Validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      errors.email = "Email already exists";
-      return res.status(400).json(errors);
+      return res.status(400).json({ email: "Email already exists" });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size
@@ -114,7 +101,7 @@ router.post("/login", (req, res) => {
 // @access  Private
 router.get(
   "/current",
-  passport.authenticate("jwt", { session: false }),
+  //passport.authenticate("jwt", { session: false }),
   (req, res) => {
     res.json({
       id: req.user.id,
