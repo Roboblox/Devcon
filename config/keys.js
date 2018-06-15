8ea490c1a64a41d1587f8f0e1bdeb37275a6b365
neojohnny <schulerjohnthomas@gmail.com>
2018-06-14 19:30:34 -0700

    Prepared for Deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://roboblox:marDUKE69@ds231205.mlab.com:31205/mern-comment-box",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
